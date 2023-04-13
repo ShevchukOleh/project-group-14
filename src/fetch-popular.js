37152e03afe0f41d0e9ad4ccbb5b11d9e9d449e3
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-14 00:13:54 +0300

    Update fetch-popular.js

--- src/fetch-popular.js
+++ src/fetch-popular.js
@@ -2,14 +2,15 @@ import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
 
 const galleryEl = document.querySelector('.gallery');
-
 const container = document.getElementById('tui-pagination-container');
+
 const instance = new Pagination(container, {
-  totalItems: 500,
+  totalItems: 10000,
   itemsPerPage: 20,
   visiblePages: 5,
   centerAlign: true,
 });
+
 let pageNumber = 1;
 
 const BASE_URL = 'https://api.themoviedb.org/3/';
@@ -17,27 +18,34 @@ const API_KEY = '404ca53f902a08bf3140e0fd0ad0a560';
 const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/';
 const IMAGE_SIZE = 'w200';
 
-fetchPopularMovies().then(data => {
+fetchPopularMovies()
+  .then(data => {
     const { page, results, total_pages, total_results } = data;
-    console.log(data);
-  renderGallery(results);
-}).catch(console.log);
+    
+    return renderGallery(results);
+  })
+  .then(res => {
+    return (galleryEl.innerHTML = res);
+  })
+  .catch(console.log);
 
 container.addEventListener('click', handleTuiContainerClick);
 
 function handleTuiContainerClick(event) {
-    pageNumber = instance.getCurrentPage();
-    console.log(pageNumber);
-    fetchPopularMovies(pageNumber).then(data => {
-        const { page, results, total_pages, total_results } = data;
-        console.log(data);
+  pageNumber = instance.getCurrentPage();
+  fetchPopularMovies(pageNumber)
+    .then(data => {
+      const { page, results, total_pages, total_results } = data;
+    
+      renderGallery(results);
     })
+    .catch(console.log);
 }
 
 function fetchPopularMovies(pageNumber) {
   const searchParams = new URLSearchParams({
-      api_key: `${API_KEY}`,
-      page: `${pageNumber}`,
+    api_key: `${API_KEY}`,
+    page: `${pageNumber}`,
   });
 
   const url = `${BASE_URL}movie/popular?${searchParams}`;
@@ -50,34 +58,61 @@ function fetchPopularMovies(pageNumber) {
   });
 }
 
+async function renderGallery(movies) {
+  const genres = await fetchGenres();
 
+  return movies
+    .map(({ genre_ids, poster_path, title, original_title, release_date }) => {
+      const checkGenres = genre_ids ? getGenres(genre_ids, genres) : 'Unknown';
 
-function renderGallery(results) {
-  const markup = results
-      .map(result => {
       return `<div class="movie-card">
-      <img class="movie-poster" src="${BASE_IMAGE_URL}${IMAGE_SIZE}${result.poster_path}" alt="${result.title}" loading="lazy" />
+      <img class="movie-poster"
+        src="${BASE_IMAGE_URL}${IMAGE_SIZE}${poster_path}" 
+        alt="${title}" 
+        loading="lazy" />
       <div class="movie-info">
       <p class="movie-title">
-      ${result.original_title}</p>
+      ${original_title}</p>
+      <p class="movie-genres">${checkGenres}</p>
+      <p class="movie-date">
+      ${release_date.slice(0, 4)}</p>
       </div>`;
     })
     .join('');
-  galleryEl.innerHTML = markup;
 }
 
-function fetchMovies(searchQuery) {
+function fetchGenres() {
   const searchParams = new URLSearchParams({
     api_key: `${API_KEY}`,
-    query: `${searchQuery}`,
+    language: 'en-US',
   });
 
-  const url = `${BASE_URL}search/movie?${searchParams}`;
+  const url = `${BASE_URL}genre/movie/list?${searchParams}`;
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => data.genres);
+}
 
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
+function getGenres(arrayId, genres) {
+  const arr = [];
+
+  for (const value of genres) {
+    if (arrayId === 'N/A' || arrayId.length === 0) {
+      arr.push('Other');
+      break;
+    } else if (arrayId.includes(value.id)) {
+      arr.push(value.name);
     }
-    return response.json();
-  });
+  }
+
+  if (arr.length > 2) {
+    arr.splice(2, arr.length - 2, 'Other');
+  }
+
+  return arr.join(', ');
 }
