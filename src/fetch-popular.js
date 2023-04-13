f306a4d0b7060d49d0daf38e1614460a992719ba
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-13 15:25:43 +0300

    Update fetch-popular.js

--- src/fetch-popular.js
+++ src/fetch-popular.js
@@ -2,19 +2,41 @@ import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
 
 const galleryEl = document.querySelector('.gallery');
-const paginationContainerEl = document.querySelector('#tui-pagination-container');
+
+const container = document.getElementById('tui-pagination-container');
+const instance = new Pagination(container, {
+  totalItems: 500,
+  itemsPerPage: 20,
+  visiblePages: 5,
+  centerAlign: true,
+});
+let pageNumber = 1;
 
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '404ca53f902a08bf3140e0fd0ad0a560';
 const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/';
 const IMAGE_SIZE = 'w200';
 
-function fetchPopularMovies() {
+fetchPopularMovies().then(data => {
+    const { page, results, total_pages, total_results } = data;
+    console.log(data);
+  renderGallery(results);
+}).catch(console.log);
+
+container.addEventListener('click', handleTuiContainerClick);
+
+function handleTuiContainerClick(event) {
+    pageNumber = instance.getCurrentPage();
+    console.log(pageNumber);
+    fetchPopularMovies(pageNumber);
+}
+
+function fetchPopularMovies(pageNumber) {
   const searchParams = new URLSearchParams({
       api_key: `${API_KEY}`,
-      page: 1,
+      page: `${pageNumber}`,
   });
-    console.log(searchParams);
+
   const url = `${BASE_URL}movie/popular?${searchParams}`;
 
   return fetch(url).then(response => {
@@ -25,14 +47,11 @@ function fetchPopularMovies() {
   });
 }
 
-fetchPopularMovies().then(data => {
-  const { page, results, total_pages, total_results } = data;
-  renderGallery(results);
-});
+
 
 function renderGallery(results) {
   const markup = results
-    .map(result => {
+      .map(result => {
       return `<div class="movie-card">
       <img class="movie-poster" src="${BASE_IMAGE_URL}${IMAGE_SIZE}${result.poster_path}" alt="${result.title}" loading="lazy" />
       <div class="movie-info">
@@ -41,18 +60,8 @@ function renderGallery(results) {
       </div>`;
     })
     .join('');
-  galleryEl.insertAdjacentHTML('beforeend', markup);
+  galleryEl.innerHTML = markup;
 }
-
-const container = document.getElementById('tui-pagination-container');
-const instance = new Pagination(container, {
-  totalItems: 500,
-  itemsPerPage: 20,
-  visiblePages: 5,
-  centerAlign: true,
-});
-const pageNumber = instance.getCurrentPage();
-console.log(pageNumber);
 
 function fetchMovies(searchQuery) {
   const searchParams = new URLSearchParams({
