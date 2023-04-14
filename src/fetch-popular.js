5c78293c55926f415d0ec5ddb5de060c8a6c6abf
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-14 17:25:22 +0300

    Update fetch-popular.js

--- src/fetch-popular.js
+++ src/fetch-popular.js
@@ -1,4 +1,10 @@
-export default function fetchPopularMovies(api_key, base_url, pageNumber) {
+import axios from 'axios';
+
+export default async function fetchPopularMovies(
+  api_key,
+  base_url,
+  pageNumber
+) {
   const searchParams = new URLSearchParams({
     api_key: `${api_key}`,
     page: `${pageNumber}`,
@@ -6,11 +12,8 @@ export default function fetchPopularMovies(api_key, base_url, pageNumber) {
   });
 
   const url = `${base_url}/movie/popular?${searchParams}`;
+  const dataObj = await axios.get(url);
+  const { data } = dataObj;
 
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  return data;
 }
