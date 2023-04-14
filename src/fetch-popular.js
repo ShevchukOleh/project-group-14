61c68564bf883c3b9b114d52eb98e814ba31215d
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-14 11:40:54 +0300

    Update fetch-popular.js

--- src/fetch-popular.js
+++ src/fetch-popular.js
@@ -16,6 +16,7 @@ let pageNumber = 1;
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '404ca53f902a08bf3140e0fd0ad0a560';
 const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/';
+const NO_POSTER = `https://i.ibb.co/r76r6Vt/oie-30214851-Ms-Wl-PTS0.png`;
 const IMAGE_SIZE = 'w200';
 
 fetchPopularMovies()
@@ -77,11 +78,16 @@ async function renderGallery(movies) {
         const checkGenres = genre_ids
           ? getGenres(genre_ids, genres)
           : 'Unknown';
-        const releaseDate = release_date ? release_date.slice(0, 4) : 'Unknoun';
-
+        const releaseDate = release_date
+          ? release_date.slice(0, 4)
+          : 'Unknoun';
+        const poster = poster_path
+          ? `${BASE_IMAGE_URL}${IMAGE_SIZE}${poster_path}`
+          : NO_POSTER;
+        
         return `<div class="movie-card">
       <img class="movie-poster"
-        src="${BASE_IMAGE_URL}${IMAGE_SIZE}${poster_path}" 
+        src="${poster}" 
         alt="${title}" 
         loading="lazy" />
       <div class="movie-info">
