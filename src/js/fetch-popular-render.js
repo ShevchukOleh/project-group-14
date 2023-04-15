1d8fd0e4f0109e5168cce1361b27905b647ebf34
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-15 18:55:09 +0300

    Update fetch-popular-render.js

--- src/js/fetch-popular-render.js
+++ src/js/fetch-popular-render.js
@@ -2,14 +2,10 @@ import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
 import fetchPopularMovies from './fetch-popular';
 import renderGallery from './render-gallery';
-//import getGenres from './get-genres';
 import setScrollToUp from './set-scroll';
-//import fetchGenres from './fetch-genres';
 
 export const BASE_URL = 'https://api.themoviedb.org/3/';
 export const API_KEY = '404ca53f902a08bf3140e0fd0ad0a560';
-//const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/';
-//const NO_POSTER = `https://i.ibb.co/r76r6Vt/oie-30214851-Ms-Wl-PTS0.png`;
 
 const moviesEl = document.querySelector('.films');
 const container = document.getElementById('tui-pagination-container');
