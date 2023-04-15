60b5a96ecaecc0419dd705414820852a7de3cfec
Inna Dmytrenko <inna.shalun@gmail.com>
2023-04-15 15:15:31 +0300

    [FT-18_FT-19] - Додала правки для кнопок в модальному вікні

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,7 @@
 import * as basicLightbox from 'basiclightbox';
 import '../sass/_modall.scss';
 import handleModal from './functionHandleModal';
+import { initModalButtonsHandler } from './modalControl';
 
 import MoviesApiService from './movies_service';
 const moviesApiService = new MoviesApiService();
@@ -9,7 +10,26 @@ bodyEl.addEventListener('click', largeMovieItem);
 
 const Handlebars = require('handlebars');
 const template = Handlebars.compile(
-  "<div class='modal modal-click'><div class='modal__container-picture'><img class='modal__picture' src={{img}} alt='icon-film'/></div><div><h2 class='modal__title'>{{title}}</h2><div class='modal__char'><span class='modal__char-name'>Vote / Votes</span><p class='modal__char-api'><span class='modal__char-api modal__vote'>{{vote}}</span> / <span class='modal__char-api'>{{votes}}</span></p></div><p class='modal__char'><span class='modal__char-name'>Popularity</span><span class='modal__char-api'>{{popularity}}</span></p><p class='modal__char'><span class='modal__char-name'>Original Title</span><span class='modal__char-api'>{{origin_title}}</span></p><p class='modal__char modal__char-mb'><span class='modal__char-name'>Genre</span><span class='modal__char-api'>{{genres}}</span></p><p class='modal__about-title'>About</p><p class='modal__about-text'>{{about}}</p><div class='modal__btn-container'><button class='modal__btn modal__btn-mr' type='button' data-id={{id}}>ADD TO WATCHED</button><button class='modal__btn' type='button' data-id={{id}}>ADD TO QUEUE</button></div><button class='modal__close' type='button'><svg width='30' height='30'><use class='modal__close-icon' href='./symbol-defs.svg#icon-close_40px'></use></svg></button></div></div>"
+  `<div class='modal modal-click'>
+    <div class='modal__container-picture'>
+      <img class='modal__picture' src={{img}} alt='icon-film'/>
+    </div>
+    <div>
+      <h2 class='modal__title'>{{title}}</h2>
+      <div class='modal__char'><span class='modal__char-name'>Vote / Votes</span>
+        <p class='modal__char-api'><span class='modal__char-api modal__vote'>{{vote}}</span> / <span class='modal__char-api'>{{votes}}</span></p>
+      </div>
+      <p class='modal__char'><span class='modal__char-name'>Popularity</span><span class='modal__char-api'>{{popularity}}</span></p>
+      <p class='modal__char'><span class='modal__char-name'>Original Title</span><span class='modal__char-api'>{{origin_title}}</span></p>
+      <p class='modal__char modal__char-mb'><span class='modal__char-name'>Genre</span><span class='modal__char-api'>{{genres}}</span></p>
+      <p class='modal__about-title'>About</p><p class='modal__about-text'>{{about}}</p>
+      <div class='modal__btn-container'>
+        <button class='modal__btn modal__btn-mr' type='button' data-btn-type="modal-btn-watched" data-id={{id}}>ADD TO WATCHED</button>
+        <button class='modal__btn' type='button' data-btn-type="modal-btn-queue" data-id={{id}}>ADD TO QUEUE</button>
+      </div>
+      <button class='modal__close' type='button'><svg width='30' height='30'><use class='modal__close-icon' href='./symbol-defs.svg#icon-close_40px'></use></svg></button>
+    </div>
+  </div>`
 );
 
 async function largeMovieItem(event) {
@@ -53,7 +73,9 @@ async function largeMovieItem(event) {
         const render = template(info);
         const instance = basicLightbox.create(render);
 
-        instance.show();
+        instance.show(() => {
+          initModalButtonsHandler()
+        });
         const ref = {
           modal: document.querySelector('.basicLightbox__placeholder'),
         };
